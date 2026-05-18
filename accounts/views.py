586b1ad5e817c4a1ca3094e8ae4b6bb3379b30ba
Josue Nehemias <r01120h@upla.edu.pe>
2026-05-18 12:24:34 -0500

    feat(accounts): add login form template

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,12 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def login_view(request):
-    return HttpResponse("Login del Sistema SGA")
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        return HttpResponse(f"Usuario: {username} | Password: {password}")
+
+    return render(request, 'accounts/login.html')
