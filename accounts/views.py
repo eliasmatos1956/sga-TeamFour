acf7501b853098e922d0d6c6937ef7e8ee2692b3
Josue Nehemias <r01120h@upla.edu.pe>
2026-05-23 18:36:31 -0500

    feat(accounts): add modern academic login UI

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 def login_view(request):
 
     if request.method == 'POST':
+
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        return HttpResponse(f"Usuario: {username} | Password: {password}")
+        return HttpResponse(
+            f"Usuario: {username} | Password: {password}"
+        )
 
     return render(request, 'accounts/login.html')
